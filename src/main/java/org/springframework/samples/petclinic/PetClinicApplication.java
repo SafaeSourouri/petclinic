7f23c53c8dba05b2b3993313e8a95c725371652c
s0130575 <Sourouri01@gmail.com>
2023-07-08 16:13:24 +0100

    change main

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -38,7 +38,7 @@ public class PetClinicApplication {
 		System.out.println(" Build  > maven");
 		System.out.println(" Test >  junit ");
 		System.out.println("build -> image");
-		System.out.println("build -> image");
+
 
 
 	}
